af9f676d5d6724a93a79a38a8fda1f230f87e839
Harlan Evans <harlanevans5@gmail.com>
2020-03-20 11:24:01 -0600

    change register route path to register-staff - can still access that page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
   belongs_to :school
   has_many :students, dependent: :destroy
-  has_many :tasks, dependent: :destroy
+  # has_many :tasks, dependent: :destroy
 end
